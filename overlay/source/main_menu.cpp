b146d3ddcf079a50022e60287a9ce6aa5c9a9894
Dawid <77678316+HackZy01@users.noreply.github.com>
2026-02-08 21:11:02 +0100

    Update main_menu.cpp

--- overlay/source/main_menu.cpp
+++ overlay/source/main_menu.cpp
@@ -26,7 +26,6 @@ tsl::elm::Element* MainMenu::createUI()
     auto frame = new tsl::elm::OverlayFrame("NX-FanControl", "v1.0.3");
     auto list = new tsl::elm::List();
 
-    // Toggle
     this->_enabledBtn->setStateChangedListener([this](bool state) {
         if (state) {
             CreateB2F();
@@ -41,7 +40,6 @@ tsl::elm::Element* MainMenu::createUI()
     });
     list->addItem(this->_enabledBtn);
 
-    // Master Slider
     this->_masterSlider->setProgress((int)(this->_fanCurveTable->fanLevel_f * 100) / 5);
     this->_masterSlider->setValueChangedListener([this](u8 value) {
         float newLevel = (float)(value * 5) / 100.0f;
@@ -54,7 +52,6 @@ tsl::elm::Element* MainMenu::createUI()
 
     list->addItem(new tsl::elm::CategoryHeader("Fan Curve", true));
 
-    // Point 0
     this->_p0Label->setClickListener([this](uint64_t keys) {
         if (keys & HidNpadButton_A) {
             tsl::changeTo<SelectMenu>(0, this->_fanCurveTable, &this->_tableIsChanged);
@@ -64,7 +61,6 @@ tsl::elm::Element* MainMenu::createUI()
     });
     list->addItem(this->_p0Label);
 
-    // Point 1
     this->_p1Label->setClickListener([this](uint64_t keys) {
         if (keys & HidNpadButton_A) {
             tsl::changeTo<SelectMenu>(1, this->_fanCurveTable, &this->_tableIsChanged);
@@ -74,7 +70,6 @@ tsl::elm::Element* MainMenu::createUI()
     });
     list->addItem(this->_p1Label);
 
-    // Point 2
     this->_p2Label->setClickListener([this](uint64_t keys) {
         if (keys & HidNpadButton_A) {
             tsl::changeTo<SelectMenu>(2, this->_fanCurveTable, &this->_tableIsChanged);
@@ -84,7 +79,6 @@ tsl::elm::Element* MainMenu::createUI()
     });
     list->addItem(this->_p2Label);
 
-    // Point 3
     this->_p3Label->setClickListener([this](uint64_t keys) {
         if (keys & HidNpadButton_A) {
             tsl::changeTo<SelectMenu>(3, this->_fanCurveTable, &this->_tableIsChanged);
@@ -94,11 +88,33 @@ tsl::elm::Element* MainMenu::createUI()
     });
     list->addItem(this->_p3Label);
 
-    // Point 4
     this->_p4Label->setClickListener([this](uint64_t keys) {
         if (keys & HidNpadButton_A) {
             tsl::changeTo<SelectMenu>(4, this->_fanCurveTable, &this->_tableIsChanged);
             return true;
+        }
+        return false;
+    });
+    list->addItem(this->_p4Label);
+
+    frame->setContent(list);
+    return frame;
+}
+
+void MainMenu::update()
+{
+    if(this->_tableIsChanged)
+    {
+        this->_p0Label->setText("P0: " + std::to_string(this->_fanCurveTable->temperature_c) + "C | " + std::to_string((int)(this->_fanCurveTable->fanLevel_f * 100)) + "%");
+        this->_p1Label->setText("P1: " + std::to_string((this->_fanCurveTable + 1)->temperature_c) + "C | " + std::to_string((int)((this->_fanCurveTable + 1)->fanLevel_f * 100)) + "%");
+        this->_p2Label->setText("P2: " + std::to_string((this->_fanCurveTable + 2)->temperature_c) + "C | " + std::to_string((int)((this->_fanCurveTable + 2)->fanLevel_f * 100)) + "%");
+        this->_p3Label->setText("P3: " + std::to_string((this->_fanCurveTable + 3)->temperature_c) + "C | " + std::to_string((int)((this->_fanCurveTable + 3)->fanLevel_f * 100)) + "%");
+        this->_p4Label->setText("P4: " + std::to_string((this->_fanCurveTable + 4)->temperature_c) + "C | " + std::to_string((int)((this->_fanCurveTable + 4)->fanLevel_f * 100)) + "%");
+        
+        WriteConfigFile(this->_fanCurveTable);
+        this->_tableIsChanged = false;
+    }
+}
         }
         return false;
     });
