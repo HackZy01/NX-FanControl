477df99158f641180b6ca04dc18e02c7bf1ad811
Dawid <77678316+HackZy01@users.noreply.github.com>
2026-02-08 19:33:09 +0100

    Remove unused variables from MainMenu class

--- overlay/source/main_menu.hpp
+++ overlay/source/main_menu.hpp
@@ -1,3 +1,4 @@
+#pragma once
 #include <tesla.hpp>
 #include <fancontrol.h>
 #include "utils.hpp"
@@ -6,20 +7,11 @@ class MainMenu : public tsl::Gui
 {
 private:
     TemperaturePoint* _fanCurveTable;
-    bool _tableIsChanged;
-
     tsl::elm::ToggleListItem* _enabledBtn;
-
-    tsl::elm::ListItem* _p0Label;
-    tsl::elm::ListItem* _p1Label;
-    tsl::elm::ListItem* _p2Label;
-    tsl::elm::ListItem* _p3Label;
-    tsl::elm::ListItem* _p4Label;
 
 public:
     MainMenu();
 
     virtual tsl::elm::Element* createUI() override;
-
     virtual void update() override;
 };
