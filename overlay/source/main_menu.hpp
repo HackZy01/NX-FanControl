f0ae55bf1bbd52d7925ee106c0ba094f5947f469
Dawid <77678316+HackZy01@users.noreply.github.com>
2026-02-08 20:32:43 +0100

    Update main_menu.hpp

--- overlay/source/main_menu.hpp
+++ overlay/source/main_menu.hpp
@@ -7,7 +7,14 @@ class MainMenu : public tsl::Gui
 {
 private:
     TemperaturePoint* _fanCurveTable;
+    bool _tableIsChanged = false;
+
     tsl::elm::ToggleListItem* _enabledBtn;
+    tsl::elm::ListItem* _p0Label;
+    tsl::elm::ListItem* _p1Label;
+    tsl::elm::ListItem* _p2Label;
+    tsl::elm::ListItem* _p3Label;
+    tsl::elm::ListItem* _p4Label;
 
 public:
     MainMenu();
